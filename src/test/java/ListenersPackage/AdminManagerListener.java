271275c69957383346a85f1c66536b313ba730d4
tbhattacharjee-deloitte <tbhattacharjee-deloitte@users.noreply.github.com>
2022-04-21 14:59:06 +0530

    Adding Listener, extent report, and logger functionality

--- src/test/java/ListenersPackage/AdminManagerListener.java
+++ src/test/java/ListenersPackage/AdminManagerListener.java
@@ -1,33 +1,35 @@
-package Listeners;
+package ListenersPackage;
 
 import TestClasses.AdminManagerTest;
+import com.aventstack.extentreports.Status;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class AdminManagerListner extends AdminManagerTest implements ITestListener {
+public class AdminManagerListener extends AdminManagerTest implements ITestListener {
     @Override
     public void onTestStart(ITestResult result) {
+        test = extent.createTest("Create User");
+        test.log(Status.INFO, "Starting " + result.getMethod().getMethodName() + " Test");
         ITestListener.super.onTestStart(result);
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
+        test.log(Status.PASS, result.getMethod().getMethodName() + " passed");
         ITestListener.super.onTestSuccess(result);
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+        test.log(Status.PASS, result.getMethod().getMethodName() + " failed");
         ITestListener.super.onTestFailure(result);
     }
 
     @Override
-    public void onStart(ITestContext context) {
-        ITestListener.super.onStart(context);
-    }
-
-    @Override
     public void onFinish(ITestContext context) {
+        test.log(Status.INFO, "Finished Main Test");
+        extent.flush();
         ITestListener.super.onFinish(context);
     }
 }
--- src/test/java/TestClasses/AdminManagerTest.java
+++ src/test/java/TestClasses/AdminManagerTest.java
@@ -1,24 +1,44 @@
 package TestClasses;
 
 import Base.BaseClass;
+import ListenersPackage.AdminManagerListener;
 import Page.Login;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
+@Listeners(AdminManagerListener.class)
 public class AdminManagerTest {
     WebDriver driver;
     By profileDivPath = By.xpath("//div[@class='profile']");
+    public ExtentReports extent;
+    public ExtentTest test;
+    public Logger logger;
+
+    public AdminManagerTest() {
+        extent = new ExtentReports();
+        logger = LogManager.getLogger(AdminManagerTest.class.getName());
+        extent.attachReporter(new ExtentHtmlReporter("extent.html"));
+    }
 
     @BeforeTest
     void login() {
+        // initialize driver
         driver = BaseClass.init();
+        // login
         Login.login(driver,"mentorAdmin", "abc123");
+        // refresh
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(profileDivPath));
         driver.navigate().refresh();
