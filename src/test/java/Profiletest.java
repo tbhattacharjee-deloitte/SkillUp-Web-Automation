032e95561af7d46f250dec1d5cf8eaf8dd6545d6
udayprsingh <udayprsingh@deloitte.com>
2022-04-25 17:41:30 +0530

    profile module added

--- src/test/java/Profiletest.java
+++ src/test/java/Profiletest.java
@@ -1,24 +1,23 @@
 import Base.BaseClass;
 import Page.Login;
+import Page.Profile;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class TestClass {
+public class Profiletest {
     WebDriver driver;
     @BeforeTest
     void login() throws InterruptedException {
         driver = BaseClass.init();
         Login.login(driver,BaseClass.prop.getProperty("username"), BaseClass.prop.getProperty("password"));
-
     }
     @Test
-    void test() throws InterruptedException {
-        Assert.assertEquals(Login.actualtext(driver),BaseClass.prop.getProperty("name"));
-        System.out.println("Assertion Passed");
-
+    void viewprofile() {
+        Profile.profileview(driver);
+        Assert.assertEquals(Profile.actualname,Profile.expectedname);
     }
 
     @AfterTest
